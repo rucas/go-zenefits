54be86834f9736611fc9d9aedcf7162fc2645688
Lucas Rondenet <lucas.rondenet@gmail.com>
2018-03-01 09:48:41 -0800

    companies and departments (#3)
    * companies and departments wired up
    * Rough draft of EmploymentsService

--- zenefits/employments.go
+++ zenefits/employments.go
@@ -1,1 +1,22 @@
 package zenefits
+
+type EmploymentsService service
+
+type Employments struct {
+	Object              string `json:"object"`
+	Person              Ref    `json:"person"`
+	Url                 string `json:"url"`
+	CompType            string `json:"comp_type"`
+	EmploymentType      string `json:"employment_type"`
+	PayRate             string `json:"pay_rate"` // TODO: not sure if this is null all the time
+	TerminationType     string `json:"termination_type"`
+	AnnualSalary        string `json:"annual_salary"`
+	TerminationDate     string `json:"termination_date"`
+	HireDate            string `json:"hire_date"`
+	Id                  string `json:"id"`
+	WorkingHoursPerWeek string `json:"working_hours_per_week"`
+}
+
+type EmploymentsFilters struct {
+	Person int `url:"person,omitempty"`
+}
